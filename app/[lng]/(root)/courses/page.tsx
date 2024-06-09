ebc4fb8820dd8497e37e4a480defca5b958aa0dc
Sardor Mahmudov <ozdemeir09@gmail.com>
2024-06-09 22:10:56 +0500

    all course

--- app/[lng]/(root)/courses/page.tsx
+++ app/[lng]/(root)/courses/page.tsx
@@ -1,11 +1,20 @@
 import TopBar from '@/components/shared/top-bar'
 import AllCourses from './_components/all-courses'
+import { SearchParamsProps } from '@/app.types'
+import { getAllCourses } from '@/actions/course.action'
 
-function Page() {
+async function Page({ searchParams }: SearchParamsProps) {
+	const resultJSON = await getAllCourses({
+		searchQuery: searchParams.q,
+		filter: searchParams.filter,
+		page: searchParams.page ? +searchParams.page : 1,
+	})
+
+	const result = JSON.parse(JSON.stringify(resultJSON))
 	return (
 		<>
 			<TopBar label='allCourses' description='allCourseDescription' />
-			<AllCourses />
+			<AllCourses result={result} />
 		</>
 	)
 }
