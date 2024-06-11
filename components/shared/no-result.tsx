ffdae60d5ea1405a5b42026ebed84ab6923d6e80
Sardor Mahmudov <ozdemeir09@gmail.com>
2024-06-11 16:06:47 +0500

    fixed errors

--- components/shared/no-result.tsx
+++ components/shared/no-result.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 interface Props {
 	title: string
 	description: string
@@ -8,13 +6,6 @@ interface Props {
 function NoResult({ description, title }: Props) {
 	return (
 		<div className='mt-10 flex w-full flex-col items-center justify-center'>
-			{/* <Image
-				src='/assets/images/not-found.png'
-				alt='No result illustration'
-				width={270}
-				height={200}
-				className='block object-contain'
-			/> */}
 			<h2 className='mt-8 font-space-grotesk text-2xl font-bold'>{title}</h2>
 			<p className='my-3.5 max-w-md text-center'>{description}</p>
 		</div>
