0c3145d9fc2d873322ca06eecdfee55f612d52be
Sardor Mahmudov <ozdemeir09@gmail.com>
2024-06-18 16:06:02 +0500

    Sections & Lessons

--- app.types.ts
+++ app.types.ts
@@ -40,8 +40,13 @@ export interface ILesson {
 		minutes: number
 		seconds: number
 	}
+	userProgress: IUserProgress[]
 }
-
+export interface IUserProgress {
+	userId: string
+	lessonId: string
+	isCompleted: string
+}
 export interface IUser {
 	_id: string
 	clerkId: string
